034e2e3a1d10cc1257230c5c0707d8758cb3bb68
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2016-11-30 11:37:39 -0500

    update service worker config to properly cache offline dependencies

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -12,7 +12,19 @@ module.exports = {
     '/index.html',
     '/manifest.json',
     '/bower_components/webcomponentsjs/webcomponents-lite.min.js',
-    '/bower_components/emojilib/emojis.json'
+    '/bower_components/emojilib/emojis.json',
+    // Precache offline support deps
+    // https://github.com/notwaldorf/mojibrag/issues/49
+    '/bower_components/app-storage/app-indexeddb-mirror/app-indexeddb-mirror-worker.js',
+    '/bower_components/app-storage/app-indexeddb-mirror/common-worker-scope.js',
+    '/bower_components/app-storage/app-indexeddb-mirror/common-worker.html',
+  ],
+  // Enable retrieval of cached common-scope-worker on browsers lacking SharedWorker
+  // The worker is fetched as "https://.../common-scope-worker.js?.../app-indexeddb-mirror.js"
+  // the arg needs to be stripped so we hit the cache
+  // https://github.com/notwaldorf/mojibrag/issues/49
+  ignoreUrlParametersMatching: [
+    /app-indexeddb-mirror/
   ],
   navigateFallback: '/index.html',
   navigateFallbackWhitelist: [/^(?!\/__)/, ' /getProjectConfig/'],
